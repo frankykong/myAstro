37edd7b9be23193b0cec4eb18b268f0f512c8f33
frankykong <frankykong@gmail.com>
2024-07-21 23:04:23 +0800

    modified:   lua/community.lua 	modified:   lua/plugins/astrocore.lua 	modified:   lua/plugins/astrolsp.lua 	modified:   lua/plugins/astroui.lua 	modified:   lua/plugins/mason.lua 	modified:   lua/plugins/none-ls.lua 	modified:   lua/plugins/treesitter.lua 	modified:   lua/plugins/user.lua

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
@@ -40,7 +40,7 @@ return {
   },
 
   -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
+  { "max397574/better-escape.nvim", enabled = true },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
@@ -81,5 +81,88 @@ return {
         Rule("a", "a", "-vim")
       )
     end,
+  },
+
+  -- add by me
+
+  -- TODO: do something
+  -- FIX: this should be fixed
+  -- HACK: weird code warning
+  --
+  -- :TodoQuickFix command brings up a quickfix list of all the TODOs in the project.
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    -- config = function()
+    --   require("todo-comments").setup {}
+    -- end,
+    -- the down beload way compare to uper whick automatically calls the
+    -- plugin's setup method with the arguments passed
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+  },
+
+  { -- Autocomplete for commands
+    "hrsh7th/cmp-cmdline",
+    keys = { ":", "/", "?" }, -- lazy load cmp on more keys along with insert mode
+    dependencies = { "hrsh7th/nvim-cmp" },
+    opts = function()
+      local cmp = require "cmp"
+      return {
+        {
+          type = "/",
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = {
+            { name = "buffer" },
+          },
+        },
+        {
+          type = ":",
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = cmp.config.sources({
+            { name = "path" },
+          }, {
+            {
+              name = "cmdline",
+              option = {
+                ignore_cmds = { "Man", "!" },
+              },
+            },
+          }),
+        },
+      }
+    end,
+    config = function(_, opts)
+      local cmp = require "cmp"
+      vim.tbl_map(function(val) cmp.setup.cmdline(val.type, val) end, opts)
+    end,
+  },
+
+  { -- a char seach and jump tool
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+
+    -- { -- AI
+    --   "Exafunction/codeium.vim",
+    --   event = "BufEnter",
+    --   config = function ()
+    --   -- Change '<C-g>' here to any keycode you like.
+    --     vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+    --     vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+    --     vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+    --     vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    --   end
+    -- },
   },
 }
